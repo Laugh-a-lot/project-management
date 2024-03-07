a201d87873cd8bb7d783c72d79a6a495557a0801
Satyam <satyam.kumar@climateconnect.digital>
2024-03-08 00:43:57 +0530

    added envs

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,4 @@
-import { SSTConfig } from "sst";
+import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
@@ -10,13 +10,15 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL!,
+          
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
-        environment: {
-          DATABASE_URL: process.env.DATABASE_URL,
-        }
       });
     });
   },
